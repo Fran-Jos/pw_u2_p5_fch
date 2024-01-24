cc9f117731b5d08e0250c465fc7068927c1c3d61
Fran-Jos <fjchanataxi@uce.edu.ec>
2024-01-24 18:32:07 -0500

    deber 7 , terminar lógica

--- src/components/PokemonOpciones.vue
+++ src/components/PokemonOpciones.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="options-container">
     <ul>
-      <li v-for="pokemon in pokemons" :key="pokemon.id" @click="$emit('selecciono', pokemon.id)" >{{ pokemon.nombre  }}</li>
+      <li v-for="pokemon in pokemons" :key="pokemon.id" @click="$emit('selecciono', pokemon.id)" >{{ pokemon.nombre}}</li>
     </ul>
   </div>
 </template>
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,89 +1,151 @@
 <template>
-  <h1 v-if="pokemonGanador===null">
-    Espere por favor estamos cargando el juego..........</h1>
+  <h1 v-if="pokemonGanador === null">
+    Espere por favor estamos cargando el juego..........
+  </h1>
   <div v-else>
-  <h1>Adivina el POKEMON</h1>
+    <div v-if="juego === true">
+      <h1>Adivina el POKEMON</h1>
+      <PokemonImagen
+        :pokemonId="pokemonGanador.id"
+        :showPokemon="mostrarPokemon"
+      />
+      <PokemonOpciones
+        v-on:selecciono="revisarClick($event)"
+        :pokemons="pokemonsArr"
+        @click="evaluar"
+      />
+    </div>
+    <div v-if="juego != true && gana === false">
+      <h2>Perdistes: el correcto era....</h2>
+      <PokemonImagen
+        :pokemonId="pokemonGanador.id"
+        :showPokemon="mostrarPokemon"
+      />
+      <h1>{{ pokemonGanador.nombre }}</h1>
+      <button @click="reiniciar">Volver a jugar</button>
+    </div>
 
-  <PokemonImagen :pokemonId="pokemonGanador.id" :showPokemon="false"/>
-  <PokemonOpciones v-on:selecciono="revisarClick($event)" :pokemons="pokemonsArr"/>
-</div>
+    <div v-if="juego === false && gana != false">
+      <h2>Ganaste</h2>
+      <PokemonImagen
+        :pokemonId="pokemonGanador.id"
+        :showPokemon="mostrarPokemon"
+      />
+      <h1>{{ pokemonGanador.nombre }}</h1>
+            <button @click="reiniciar">Volver a jugar</button>
+    </div>
+  </div>
+
+  <div></div>
+  <div></div>
 </template>
 
 <script>
-import PokemonImagen from "../components/PokemonImagen.vue"
-import PokemonOpciones  from "../components/PokemonOpciones.vue"
+import PokemonImagen from "../components/PokemonImagen.vue";
+import PokemonOpciones from "../components/PokemonOpciones.vue";
 
-import obtenerPokemonsFachada from "../helpers/pokemonHelper"
+import obtenerPokemonsFachada from "../helpers/pokemonHelper";
 
-obtenerPokemonsFachada()
+obtenerPokemonsFachada();
 
 export default {
-components:{
-    PokemonImagen,
-    PokemonOpciones
-},
-// fases del ciclo de vida de un componente
-beforeCreate(){
-  console.log("antes de crear el componente");
-},
+  data() {
+    return {
+      pokemonsArr: [],
+      pokemonGanador: null,
+      mostrarPokemon: false,
+      gana: null,
 
-created(){
-  console.log("Se creo el componente");
-},
-
-beforeMount(){
-  console.log("Antes de que se monye el componente en la página")
-},
-
-mounted(){
-  console.log("Se monto el componente PokemonPage")
-  this.cargaInicial()
-},
-
-beforeUpdate(){
-  console.log("Antes de que se actualice el componente");
-},
-
-update(){
-console.log("Se actualiza el componente")
-},
-
-beforeDestroy(){
-console.log("antes de destruir")
-},
-
-destroyed(){
-console.log("destruido")
-},
-
-methods:{
-  async cargaInicial(){
-  const arregloPokemons = await obtenerPokemonsFachada()
-  console.log('desde componente')
-  console.log(arregloPokemons)
-  this.pokemonsArr = arregloPokemons;
-
-  const indiceGanador=Math.floor(Math.random()*4);
-  this.pokemonGanador=  this.pokemonsArr[indiceGanador];
+      juego: true,
+    };
   },
 
-  revisarClick(datoRecivido){
-console.log("dio click y se lo emite al padre")
-console.log(datoRecivido)  
-}
-},
+  components: {
+    PokemonImagen,
+    PokemonOpciones,
+  },
+  props: {
+    supuestoGanador: null,
+  },
 
+  // fases del ciclo de vida de un componente
+  beforeCreate() {
+    console.log("antes de crear el componente");
+  },
 
-data(){
-  return{
-    pokemonsArr:[],
-    pokemonGanador:null,
-    mostrarPokemon:false
-  }
-}
-}
+  created() {
+    console.log("Se creo el componente");
+  },
+
+  beforeMount() {
+    console.log("Antes de que se monye el componente en la página");
+  },
+
+  mounted() {
+    console.log("Se monto el componente PokemonPage");
+    this.cargaInicial();
+  },
+
+  beforeUpdate() {
+    console.log("Antes de que se actualice el componente");
+  },
+
+  update() {
+    console.log("Se actualiza el componente");
+  },
+
+  beforeDestroy() {
+    console.log("antes de destruir");
+  },
+
+  destroyed() {
+    console.log("destruido");
+  },
+
+  methods: {
+    reiniciar() {
+      this.cargaInicial
+      (this.pokemonsArr = []),
+        (this.pokemonGanador = null),
+        (this.mostrarPokemon = false),
+        (this.gana = null),
+        (this.juego = true);
+        
+    },
+
+    async cargaInicial() {
+      const arregloPokemons = await obtenerPokemonsFachada();
+      console.log("desde componente");
+      console.log(arregloPokemons);
+      this.pokemonsArr = arregloPokemons;
+
+      const indiceGanador = Math.floor(Math.random() * 4);
+      this.pokemonGanador = this.pokemonsArr[indiceGanador];
+    },
+
+    revisarClick(datoRecivido) {
+      console.log("dio click y se lo emite al padre");
+      console.log(datoRecivido);
+
+      if (datoRecivido === this.pokemonGanador.id) {
+        this.mostrarPokemon = true;
+        console.log("es este el ganador");
+        this.gana = true;
+
+        this.juego = false;
+      } else {
+        console.log("te equivocaste");
+        this.gana = false;
+        this.mostrarPokemon = true;
+
+        this.juego = false;
+      }
+    },
+
+    evaluar() {},
+  },
+};
 </script>
 
 <style>
-
 </style>
